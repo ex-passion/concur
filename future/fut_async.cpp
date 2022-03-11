f940dd252f5db5bbedf12afa8c6eb2ec786be9aa
Anubhav Garg <garg.anubhav@outlook.com>
2022-03-11 22:10:21 +0530

    Experimenting on sum of natural numbers in list and vector recevied in future

--- future/fut_async.cpp
+++ future/fut_async.cpp
@@ -1,51 +1,70 @@
-#include<thread>
-#include<vector>
-#include<iostream>
-#include<numeric>
+/* 
+ * Experimenting future
+ *
+ * @Author: github.com/ex-passion 
+ * Experience Passion
+ *
+ */
+
 #include<future>
+#include<vector>
+#include<list>
+#include<queue>
+#include<iostream>
+#include<numeric> //For accumulate
 
-
-long example(std::vector<int>&& v) {
+template<typename T>
+long example(T&& t) {
 	std::this_thread::sleep_for(std::chrono::milliseconds(20));
-	return accumulate(v.begin(), v.end(),0l);
+	return accumulate(t.begin(), t.end(),0l);
 }
 
-#define MAX (100000000)  //0.4 GB
+#define MAX (10000000)  
 
-int main(){
+/* Template function for data structure sum */
+template<typename D>
+void dsSum (){
 
-	std::vector<int> vm1;
+	D d1;
 	for(int i=1; i<=MAX; ++i) {
-		vm1.emplace_back(i);
+		d1.emplace_back(i);
 	}
-	std::future<long> f1 = std::async(std::launch::async, example, move(vm1));
+	std::future<long> f1 = std::async(std::launch::async, example<D>, move(d1));
 	//std::future<int> f1 = std::thread(example, vm1).get_future();
 	//std::future<int> f1 = std::thread(example, vm1);
 
-	std::vector<int> vm2;
+	D d2;
 	for(int i=MAX; i>0; --i) {
-		vm2.emplace_back(i);
+		d2.emplace_back(i);
 	}
-	std::future<long> f2 = std::async(example, move(vm2));
+	std::future<long> f2 = std::async(example<D>, move(d2));
 
-	std::vector<int> vm3;
+	D d3;
 	for(int i=MAX; i>0; --i) {
-		vm3.emplace_back(i);
+		d3.emplace_back(i);
 	}
-	std::future<long> f3 = std::async(example, vm3);
+	std::future<long> f3 = std::async(example<D>, d3);
 
-	std::vector<int> vm4;
-	for(int i=MAX; i>0; --i) {
-		vm4.emplace_back(i);
+	D d4;
+	for(int i=1; i<=MAX; ++i) {
+		d4.emplace_back(i);
 	}
-
-	std::future<long> f4 = std::async(std::launch::deferred,example, vm4);
+	std::future<long> f4 = std::async(std::launch::deferred, example<D>, d4);
 
 	std::cout<<"Result is "<<f1.get()<<std::endl;
 	std::cout<<"Result is "<<f2.get()<<std::endl;
 	std::cout<<"Result is "<<f3.get()<<std::endl;
 	std::cout<<"Result is "<<f4.get()<<std::endl;
+}
 
+
+int main(){
+
+	dsSum<std::vector<int>>();
+	std::cout<<std::endl;
+
+	dsSum<std::list<int>>();
+	std::cout<<std::endl;
 
 	return 0;
 }
